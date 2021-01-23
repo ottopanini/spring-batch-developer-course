a7f9a024f4bff794e1ef56a22649b355b1e6b9c0
Christian Preß <cpress@it4ipm.de>
2021-01-23 10:55:33 +0100

    init transitions

---
+++ transitions/src/main/java/io/memyselfandi/transitions/TransitionsApplication.java
@@ -0,0 +1,13 @@
+package io.memyselfandi.transitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransitionsApplication.class, args);
+    }
+
+}
---
+++ transitions/src/test/java/io/memyselfandi/transitions/TransitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.memyselfandi.transitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

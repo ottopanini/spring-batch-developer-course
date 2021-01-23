87436cf7df664371a77043b3ef227b6011cff7c5
Christian Preß <cpress@it4ipm.de>
2021-01-23 14:48:52 +0100

    decisions init

---
+++ decisions/src/test/java/io/me/decisions/DecisionsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.me.decisions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

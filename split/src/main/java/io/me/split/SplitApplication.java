0ad1905c3958114a419c4fbff5840a4cd485de2a
Christian Preß <cpress@it4ipm.de>
2021-01-23 12:00:23 +0100

    add spring batch setup

--- split/src/main/java/io/me/split/SplitApplication.java
+++ split/src/main/java/io/me/split/SplitApplication.java
@@ -1,9 +1,12 @@
 package io.me.split;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableBatchProcessing
 public class SplitApplication {
 
     public static void main(String[] args) {
---
+++ split/src/main/java/io/me/split/configuration/SplitConfiguration.java
@@ -0,0 +1,7 @@
+package io.me.split.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SplitConfiguration {
+}

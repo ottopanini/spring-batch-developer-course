56dc5585d5a3aedb7531d5630c78f3f4ab67f6ca
Christian Preß <cpress@it4ipm.de>
2021-01-23 15:10:20 +0100

    nested jobs init

---
+++ nested-jobs/src/main/java/io/me/nestedjobs/NestedJobsApplication.java
@@ -0,0 +1,13 @@
+package io.me.nestedjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestedJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NestedJobsApplication.class, args);
+    }
+
+}

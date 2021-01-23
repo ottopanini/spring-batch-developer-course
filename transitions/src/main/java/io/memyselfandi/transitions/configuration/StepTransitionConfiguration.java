a408b5dd8ecd82a68189f422c0156b438fcfba7f
Christian Preß <cpress@it4ipm.de>
2021-01-23 11:05:49 +0100

    add transitions config

---
+++ transitions/src/main/java/io/memyselfandi/transitions/configuration/StepTransitionConfiguration.java
@@ -0,0 +1,55 @@
+package io.memyselfandi.transitions.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StepTransitionConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public StepTransitionConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, ctxt) -> {
+                    System.out.println(">> this is step 1");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, ctxt) -> {
+                    System.out.println(">> this is step 2");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, ctxt) -> {
+                    System.out.println(">> this is step 3");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+    @Bean
+    Job job() {
+       return jobBuilderFactory.get("job")
+               .start(step1())
+               .next(step2())
+               .next(step3())
+               .build();
+    }
+}

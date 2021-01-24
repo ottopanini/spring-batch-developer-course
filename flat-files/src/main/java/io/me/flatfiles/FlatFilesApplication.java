0d641aa5a3c34a972ff7431a506eb84796f54d90
Christian Preß <cpress@it4ipm.de>
2021-01-24 15:18:02 +0100

    read flat files init

---
+++ flat-files/src/main/java/io/me/flatfiles/FlatFilesApplication.java
@@ -0,0 +1,13 @@
+package io.me.flatfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlatFilesApplication.class, args);
+    }
+
+}

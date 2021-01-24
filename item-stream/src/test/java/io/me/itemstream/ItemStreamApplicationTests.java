344b6d7b52bd629ada7846b2192c3e09adb3dc68
Christian Preß <cpress@it4ipm.de>
2021-01-24 19:51:47 +0100

    item stream init

---
+++ item-stream/src/test/java/io/me/itemstream/ItemStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package io.me.itemstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

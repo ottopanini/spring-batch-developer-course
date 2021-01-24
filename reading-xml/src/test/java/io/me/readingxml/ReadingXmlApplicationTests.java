e8480acd978795803509c79e4262b6ab8025028f
Christian Preß <cpress@it4ipm.de>
2021-01-24 15:58:17 +0100

    reading xml init

---
+++ reading-xml/src/test/java/io/me/readingxml/ReadingXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.me.readingxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingXmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

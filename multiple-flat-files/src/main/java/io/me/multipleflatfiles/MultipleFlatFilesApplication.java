8cfebca9af4cd382e0ee03e5b6989b45f62fd760
Christian Preß <cpress@it4ipm.de>
2021-01-24 17:19:09 +0100

    multiple flat files init

---
+++ multiple-flat-files/src/main/java/io/me/multipleflatfiles/MultipleFlatFilesApplication.java
@@ -0,0 +1,13 @@
+package io.me.multipleflatfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleFlatFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleFlatFilesApplication.class, args);
+    }
+
+}
---
+++ multiple-flat-files/src/test/java/io/me/multipleflatfiles/MultipleFlatFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.me.multipleflatfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleFlatFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

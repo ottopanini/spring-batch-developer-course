504ee9b3bbb544c4a6d62a34d3d35b102f32aba0
Christian Preß <cpress@it4ipm.de>
2021-01-24 11:53:35 +0100

    job parameters init

---
+++ job-parameters/src/main/java/io/me/jobparameters/JobParametersApplication.java
@@ -0,0 +1,13 @@
+package io.me.jobparameters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobParametersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobParametersApplication.class, args);
+    }
+
+}
---
+++ job-parameters/src/test/java/io/me/jobparameters/JobParametersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.me.jobparameters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobParametersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

0c23549c9ddf8b1403bc2347100106f7ff1ce6bb
Christian Preß <cpress@it4ipm.de>
2021-01-25 19:37:08 +0100

    add flat file output

---
+++ flat-file-output/src/main/java/io/me/flatfileoutput/config/JobConfig.java
@@ -0,0 +1,80 @@
+package io.me.flatfileoutput.config;
+
+import io.me.flatfileoutput.domain.Customer;
+import io.me.flatfileoutput.domain.CustomerRowMapper;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.PassThroughLineAggregator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.util.HashMap;
+
+@Configuration
+public class JobConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+
+    public JobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    JdbcPagingItemReader<Customer> pagingItemReader() {
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+        reader.setDataSource(dataSource);
+        reader.setFetchSize(10);
+        reader.setRowMapper(new CustomerRowMapper());
+
+        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("FROM customer");
+        HashMap<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("id", Order.ASCENDING);
+        queryProvider.setSortKeys(sortKeys);
+        reader.setQueryProvider(queryProvider);
+
+        return reader;
+    }
+
+    @Bean
+    FlatFileItemWriter<Customer> customerItemWriter() throws Exception {
+        FlatFileItemWriter<Customer> itemWriter = new FlatFileItemWriter<>();
+        itemWriter.setLineAggregator(new PassThroughLineAggregator<>());
+        String path = File.createTempFile("customerOutput", ".out").getAbsolutePath();
+        System.out.println(">> to: " + path);
+        itemWriter.setResource(new FileSystemResource(path));
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+    }
+
+    @Bean
+    Step step() throws Exception {
+        return stepBuilderFactory.get("step113")
+                .<Customer, Customer>chunk(10)
+                .reader(pagingItemReader())
+                .writer(customerItemWriter())
+                .build();
+    }
+
+    @Bean
+    Job job() throws Exception {
+        return jobBuilderFactory.get("job")
+                .start(step())
+                .build();
+    }
+
+}

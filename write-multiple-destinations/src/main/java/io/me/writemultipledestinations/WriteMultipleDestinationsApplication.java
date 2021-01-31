7316e57a6dcb89452ce2c1115ad2ea0901401ad4
Christian Preß <cpress@it4ipm.de>
2021-01-31 18:11:56 +0100

    write multiple destinations init

---
+++ write-multiple-destinations/src/main/java/io/me/writemultipledestinations/WriteMultipleDestinationsApplication.java
@@ -0,0 +1,13 @@
+package io.me.writemultipledestinations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteMultipleDestinationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WriteMultipleDestinationsApplication.class, args);
+    }
+
+}
---
+++ write-multiple-destinations/src/test/java/io/me/writemultipledestinations/WriteMultipleDestinationsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.me.writemultipledestinations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteMultipleDestinationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

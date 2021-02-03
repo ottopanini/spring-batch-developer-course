d288b402f790434bbfbc908aea4f285cd0a02c99
Christian Preß <cpress@it4ipm.de>
2021-02-03 21:07:12 +0100

    skip retry listeners init

---
+++ skip-retry-listeners/src/main/java/io/me/skipretrylisteners/SkipRetryListenersApplication.java
@@ -0,0 +1,13 @@
+package io.me.skipretrylisteners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipRetryListenersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkipRetryListenersApplication.class, args);
+    }
+
+}
---
+++ skip-retry-listeners/src/test/java/io/me/skipretrylisteners/SkipRetryListenersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.me.skipretrylisteners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkipRetryListenersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

e78b5bc2e083af75bc258e75d5f23d17546191e0
Christian Preß <cpress@it4ipm.de>
2021-02-04 22:20:44 +0100

    remote partitioning init

---
+++ remote-partitioning/src/main/java/io/me/remotepartitioning/RemotePartitioningApplication.java
@@ -0,0 +1,13 @@
+package io.me.remotepartitioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotePartitioningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemotePartitioningApplication.class, args);
+    }
+
+}
---
+++ remote-partitioning/src/test/java/io/me/remotepartitioning/RemotePartitioningApplicationTests.java
@@ -0,0 +1,13 @@
+package io.me.remotepartitioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemotePartitioningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
